e046294d7d7d37acb2c5beae6906a58aff622c2c
Roan André de Souza <roanflm@gmail.com>
2025-09-27 17:12:20 -0300

    Update transformations.sql

--- sql/transformations.sql
+++ sql/transformations.sql
@@ -1,1 +1,8 @@
-
+CREATE OR REPLACE TABLE dataset_refined.energy_consumption AS
+SELECT
+  DATE(data) AS data,
+  cidade,
+  consumo_mwh,
+  consumo_mwh * 1000 AS consumo_kwh
+FROM
+  dataset_trusted.energy_consumption;
